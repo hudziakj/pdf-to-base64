11c739e0f0e5f2ac1f7aaee44df4be1247ad0ba4
Matheus Hudziak <matheuszhudziak@gmail.com>
2023-04-14 14:51:09 -0300

    Subindo primeira versão

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const puppeteer = require("puppeteer-core");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/pdf", async (req, res) => {
+  const url = req.body.url;
+
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/google-chrome-stable",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle0" });
+  const pdf = await page.pdf();
+  await browser.close();
+
+  res.contentType("application/pdf");
+  res.send(pdf);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
